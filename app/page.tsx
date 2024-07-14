5b81de890e615d633ae27f03114689afe95bfea5
dongguJeong <zzzjdg@naver.com>
2024-07-14 19:27:39 +0900

    태일윈드 연습 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,31 @@
 export default function Home() {
   return (
    <main className="bg-gray-200 h-screen flex items-center 
-   justify-center p-5 
-   dark:bg-gray-700">
-    <div className="bg-white w-full shadow-lg rounded-3xl p-5
-    max-w-screen-sm dark:bg-gray-600">
-      <div className="flex justify-between">
-        <div className="flex flex-col">
-          <span className="text-gray-600 font-semibold -mb-1 dark:text-gray-300">In transit</span>
-          <span className="text-4xl font-semibold -mb-1 dark:text-white">Coolblue</span>
+   justify-center p-5 sm:bg-gray-200
+    md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100
+    2xl:bg-purple-100
+    ">
+    <a>파란 하이퍼링크</a>
+    <div className="btn">
+
+      {["Nico","Me","You","Yourself"].map((person,index) => 
+        <div key={index} className="flex items-center 
+        gap-5  rounded-xl group">
+          <div className="size-10 rounded-full bg-blue-400"/>
+          <span className="text-lg font-medium">{person}</span>
+          <div className="size-6 flex rounded-full bg-red-500
+           text-white relative
+          items-center justify-center">
+            <span className="z-10 group-hover:text-red-500">{index}</span>
+            <div className="size-6 bg-red-500 rounded-full animate-ping absolute"></div>
+          </div>
         </div>
-        <div className="rounded-full bg-orange-400 size-12"></div>
+
        
-      </div>
-      <div className="my-2 flex items-center gap-2">
-        <span className="bg-green-400 text-white uppercase 
-        px-2.5 py-1.5 text-xs font-medium rounded-full transition hover:bg-green-500 hover:scale-125 ">
-          Today</span>
-        <span className="dark:text-gray-100">9:30-10:30u</span>
-      </div>
-      <div>
-        <div className="relative my-2">
-          <div className="bg-gray-200 w-full rounded-full h-2 absolute"/>
-          <div className="bg-green-400 w-2/3 rounded-full h-2 absolute"/>
-        </div>
-        <div className=" text-sm font-normal flex justify-between items-center
-          text-gray-500 dark:text-gray-100
-        ">
-          <span className="pt-4">Expected</span>
-          <span className="pt-4"> Sorting center</span>
-          <span className="pt-4">In transit</span>
-          <span className="pt-4 text-gray-400 dark:text-gray-500">Delivered</span>
-        </div>
-      </div>  
+      )}
     </div>
    </main>
+
+   
   );
 }
